c2104e8257e611f30dc6778b2d2db2b4114a2144
Christian Schrader <schrader@evendi.de>
2013-05-07 01:01:00 +0200

    changed change to remove add

--- db/migrate/20121226152149_fix_advertisement_table.rb
+++ db/migrate/20121226152149_fix_advertisement_table.rb
@@ -2,6 +2,7 @@ class FixAdvertisementTable < ActiveRecord::Migration
   def change
     remove_column :advertisements, :from
     remove_column :advertisements, :to
-    change_column :advertisements, :calendar_week, :integer
+    remove_column :advertisements, :calendar_week
+    add_column :advertisements, :calendar_week, :integer
   end
 end
