1f55a367d54ba10c3c536e4ae02facb1692e449f
jnillo <jnillo9@hotmail.com>
2013-05-29 00:52:43 +0200

    resolve worng method in heroku

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,5 +1,6 @@
 class WelcomeController < ApplicationController
   def index
+    redirect_to :action => "city",:city => City.default_city.name
     single_events = SingleEvent.recent_to_soon(4.weeks)
     events_by_day = Calendar.events_by_day(single_events)
     @mini_calendar_events = Calendar.fill_gaps(events_by_day, Date.today - 4.weeks, Date.today + 4.weeks)
