ebe6f41823f16e0ac178042999bb9f6508b21dfd
jnillo <jnillo9@hotmail.com>
2013-05-28 19:45:53 +0200

    remove migrate for rename city in Venue model

--- db/migrate/20130525154630_rename_column_city_in_venues.rb
+++
@@ -1,9 +0,0 @@
-class RenameColumnCityInVenues < ActiveRecord::Migration
-  def up
-    rename_column :venues, :city, :city_name
-  end
-
-  def down
-    rename_column :venues, :city_name, :city
-  end
-end
