63ba7c605503c6ecb89ef9baf96a14c7509ae6bc
Christian Schrader <schrader@evendi.de>
2013-05-31 09:34:55 +0200

    S3 upload Dir

--- app/uploaders/abstract_uploader.rb
+++ app/uploaders/abstract_uploader.rb
@@ -8,7 +8,7 @@ class AbstractUploader < CarrierWave::Uploader::Base
   storage :fog
 
   def store_dir
-    "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
+    "#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
   end
 
   def extension_white_list
--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -31,5 +31,5 @@ CarrierWave.configure do |config|
   config.fog_public = true
   config.cache_dir = "#{Rails.root}/tmp/uploads"                  # To let CarrierWave work on heroku
 
-  config.fog_directory    = ENV['S3_BUCKET_NAME']
+  config.fog_directory    = "uploads"
 end
