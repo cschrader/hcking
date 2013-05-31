9fcfa0e21309cd3ded80c7a3743af553d8335f77
Christian Schrader <schrader@evendi.de>
2013-05-31 08:49:56 +0200

    uploader fog

--- app/uploaders/abstract_uploader.rb
+++ app/uploaders/abstract_uploader.rb
@@ -4,10 +4,11 @@ class AbstractUploader < CarrierWave::Uploader::Base
   include Sprockets::Helpers::RailsHelper
   include Sprockets::Helpers::IsolatedHelper
 
-  storage :file
+#  storage :file
+  storage :fog
 
   def store_dir
-    "banane/uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
+    "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
   end
 
   def extension_white_list
