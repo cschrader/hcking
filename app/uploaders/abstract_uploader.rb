7ca328e87b81436652b911b3d1feddc39e12a0ae
Christian Schrader <schrader@evendi.de>
2013-05-31 08:11:16 +0200

    test how to change the uploads dir

--- app/uploaders/abstract_uploader.rb
+++ app/uploaders/abstract_uploader.rb
@@ -7,7 +7,7 @@ class AbstractUploader < CarrierWave::Uploader::Base
   storage :file
 
   def store_dir
-    "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
+    "banane/uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
   end
 
   def extension_white_list
