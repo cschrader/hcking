1841cad7d8a59c0139cf949f02962b278d9e06b2
Christian Schrader <schrader@evendi.de>
2013-06-20 22:47:08 +0200

    deactivated sweeper and moved the expiration of the cache to the singleeventcontroller

--- app/controllers/single_events_controller.rb
+++ app/controllers/single_events_controller.rb
@@ -10,4 +10,14 @@ class SingleEventsController < ApplicationController
     @event = @single_event.event
     respond_with @single_event
   end
+
+  def after_save(single_event)
+    expire_cache_for_single_events
+  end
+
+  private
+  def expire_cache_for_single_events
+    expire_fragment('all_events_calendar')
+  end
+  
 end
