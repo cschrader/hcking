3f31f6960896621d1be1e8cb6d4f6f9e7cf3343a
Christian Schrader <schrader@evendi.de>
2013-06-28 13:19:32 +0200

    if not city for blogposts use default city

--- app/controllers/blog_posts_controller.rb
+++ app/controllers/blog_posts_controller.rb
@@ -4,7 +4,11 @@ class BlogPostsController < ApplicationController
   before_filter :advertisement, :only => [:index, :show, :feed]
 
   def index
-    @city = City.find_by_name params[:city]
+    if params[:city]
+      @city = City.find_by_name params[:city]
+    else
+      @city = City.find_by_default true
+    end
     @posts = BlogPost.for_web.blog.order("publishable_from desc").page(params[:page]).per(10).where(:city_id => @city.id)
     find_post_by_params
   end
